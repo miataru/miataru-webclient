4b40b315a504ab07f1544acac3c1292aa969147a
Daniel Kirstenpfad <bietiekay@miataru.com>
2013-12-14 19:53:44 +0100

    initial commit with leaflet, bootstrap and stuff

---
+++ index.js
@@ -0,0 +1,25 @@
+function init() 
+{
+var map = L.map('map').setView([49.81877,10.80651], 13);
+
+		L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+		}).addTo(map);
+
+
+		L.marker([49.81877,10.80651]).addTo(map)
+			.bindPopup("<b>Hello world!</b><br />I am a popup.");
+
+		var popup = L.popup();
+
+		function onMapClick(e) {
+			popup
+				.setLatLng(e.latlng)
+				.setContent("You clicked the map at " + e.latlng.toString())
+				.openOn(map);
+		}
+
+		map.on('click', onMapClick);
+
+}
